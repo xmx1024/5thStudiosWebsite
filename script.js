853b2c04163071f07fb5dd318bfb04ce5cd1c400
Jared Furlow <jdf2.furlow@gmail.com>
2014-11-24 15:39:23 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+$("#nav").html("");
 $("#nav").append('<li><img src="images/logo/Logo_128x.png"style="width: 37px; height: 36px"></img></li>');
 $("#nav").append('<li><a href="aboutus.html">About Us</a></li>');
 $("#nav").append('<li><a href="https://github.com/5thStudios">Github</a></li>');
