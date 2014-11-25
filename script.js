187dce51eed4f8151d53fc65af1b3eaaff069577
Phase <ph4sesaber@gmail.com>
2014-11-24 19:46:34 -0800

    Update script.js
    Made 'Minecraft Plugins' 'Stream'

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ $("#nav").html(" ");
 $("#nav").append('<li><img src="images/logo/Logo_128x.png"style="width: 37px; height: 36px"></img></li>');
 $("#nav").append('<li><a href="aboutus.html">About Us</a></li>');
 $("#nav").append('<li><a href="https://github.com/5thStudios">Github</a></li>');
-$("#nav").append('<li><a href="http://plugins.5th.xyz">Minecraft Plugins</a></li>');
+$("#nav").append('<li><a href="http://plugins.5th.xyz">Stream</a></li>');
 });
