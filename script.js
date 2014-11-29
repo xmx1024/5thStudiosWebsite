2c0847ae4f19af56a6d165590686daa1a9ffcaa1
Phase <ph4sesaber@gmail.com>
2014-11-28 20:44:18 -0800

    revert

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
 $("#nav").html(" ");
-$("#nav").append('<li><a href="http://5th.xyz"><img src="images/logo/Logo_128x.png"style="width: 37px; height: 36px"></img></a></li>');
+$("#nav").append('<li><img src="images/logo/Logo_128x.png"style="width: 37px; height: 36px"></img></li>');
 $("#nav").append('<li><a href="about">About Us</a></li>');
 $("#nav").append('<li><a href="https://github.com/5thStudios">Github</a></li>');
 $("#nav").append('<li><a href="http://plugins.5th.xyz">Minecraft Plugins</a></li>');
